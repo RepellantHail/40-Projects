3aa5bd4b7db5910f72936eab656c808c1c931007
RepellantHail <a20310068@ceti.mx>
2023-05-19 20:53:48 -0600

    baseUpdated

---
+++ PracticaBaseLocal/app.js
@@ -0,0 +1,29 @@
+const alumno = [{
+    registro: 20310068,
+    nombre: 'Luis Modesto',
+    email: 'a20310068@ceti.mx',
+    grupo: '5M',
+    carrera: 'Desarrollo De Software'
+}]
+
+localStorage.setItem('alumno',JSON.stringify(alumno))
+
+
+// Retrieve data from local storage
+var data = JSON.parse(localStorage.getItem('alumno')) || [];
+
+// Generate table rows dynamically
+var rows = '';
+data.forEach(function(item) {
+  rows += '<tr>';
+  rows += '<td>' + item.registro + '</td>';
+  rows += '<td>' + item.nombre + '</td>';
+  rows += '<td>' + item.email + '</td>';
+  rows += '<td>' + item.grupo + '</td>';
+  rows += '<td>' + item.carrera + '</td>';
+  rows += '</tr>';
+});
+
+// Add generated rows to the table body
+var dataRows = document.getElementById('dataRows');
+dataRows.innerHTML = rows;
